6cac2e38ba97deb5a55a5d5fef2d74a499f26486
Adrian Booth <Adrian.booth17@gmail.com>
2016-01-14 19:05:00 +0000

    accepts only valid coins

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,13 +1,25 @@
 class VendingMachine
 
-  attr_reader :current_amount
+  attr_reader :current_amount, :coin_return, :valid_coins
 
   def initialize
     @current_amount = 0
+    @coin_return = 0
+    @valid_coins = [0.05, 0.10, 0.20, 0.50, 1.00, 2.00]
   end
 
   def accept_coins(coin)
+    if coin_invalid?(coin)
+      @coin_return = coin
+    end
+    raise "This is not a valid coin" if coin_invalid?(coin)
     @current_amount+=coin
+  end
+
+  private
+
+  def coin_invalid?(coin)
+    @valid_coins.include?(coin) ? false : true
   end
 
 end
